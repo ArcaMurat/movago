bf152dfc217ef7ba10acc644c73bcc2f6f128b9a
arcamurat <murat.akpek@ug.bilkent.edu.tr>
2024-04-17 13:40:19 +0300

    movago v0.1

---
+++ src/main/java/com/puff/movago/Movago.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.puff.movago;
+
+/**
+ *
+ * @author Arca
+ */
+public class Movago {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
